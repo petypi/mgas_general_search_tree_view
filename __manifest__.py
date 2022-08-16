5ae0481f0cd994856df42869bd25f7fddcb5e6f4
Peter <petypi@gmail.com>
2022-08-16 10:27:08 +0300

    add search view to one2many

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Fortutech IMS Pvt. Ltd.
+# Copyright (C) 2016-TODAY Fortutech IMS Pvt. Ltd.(<http://www.fortutechims.com>).
+#
+###############################################################################
+{
+    'name': 'General Search in Tree/List View',
+    'category': 'Report',
+    'summary': 'General Search in Tree/List View',
+    'version': '13.0.1.0',
+    'license': 'OPL-1',
+    'description': """User allow to search for all fields value in tree view.""",
+    'depends': ['base'],
+    'author': "Peter Mudoko.",
+    'website': "http://www.mgas.ke",
+    'data': [
+        'views/assets.xml',
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "images": ['static/description/banner.png'],
+}
